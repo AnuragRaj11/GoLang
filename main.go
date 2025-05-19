061b395288f67a34b2faf22ced3a10097d89a117
AnuragRaj11 <Araj59519@gmail.com>
2025-05-19 23:38:14 +0530

    new lang

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Go!")
+
+	var person string = "AR"
+	fmt.Println(person)
+	var balance int = 70000
+	fmt.Println(balance)
+	var isActive bool = true
+	fmt.Println(isActive)
+	var floatNum float64 = 3.14
+	fmt.Println(floatNum)
+
+	var Pulbic string = "Public"
+	fmt.Println(Pulbic)
+	var private string = "Private"
+	fmt.Println(private)
+}
