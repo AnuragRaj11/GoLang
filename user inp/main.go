ac92b42e1f3fd1d5e341c30a52848b79e67aa4c5
AnuragRaj11 <Araj59519@gmail.com>
2025-05-20 23:34:39 +0530

    input

--- user inp/main.go
+++ user inp/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Hello, what's your name?")
+	// fmt.Println("Hello, what's your name?")
 	var name string;
-	fmt.Scan(&name)
+	// fmt.Scan(&name)
+	// fmt.Println("Hello, " + name + "! How are you today?"
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ = reader.ReadString('\n')
 	fmt.Println("Hello, " + name + "! How are you today?")
 }
