5b8e9d2204e81c33d0f32a056aff3f49038d2ef8
AnuragRaj11 <Araj59519@gmail.com>
2025-05-21 23:42:27 +0530

    loops

---
+++ loops/main.go
@@ -0,0 +1,26 @@
+package main
+import
+	"fmt"
+
+	func main() {
+		x:=4
+		if x%2==0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("Odd")
+		}
+
+		day:=4
+		switch day {
+		case 1:
+			fmt.Println("Monday")
+			case 2:
+			fmt.Println("Tuesday")
+			case 3:
+			fmt.Println("Wednesday")
+			case 4:
+			fmt.Println("Thursday")
+			case 5:
+			fmt.Println("Friday")
+		}
+	}
