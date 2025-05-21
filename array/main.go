6cd27e64a06a5053843ce7190e28e42ef8b095f9
AnuragRaj11 <Araj59519@gmail.com>
2025-05-21 23:25:38 +0530

    array

---
+++ array/main.go
@@ -0,0 +1,16 @@
+package main 
+
+import 
+	"fmt"
+
+
+func main() {
+	
+	var arr = []int{1, 2, 3, 4, 5}
+	fmt.Println("Array:", arr)
+
+	var sum[4] int 
+	fmt.Println("Sum:", sum)
+
+	fmt.Println("Length of array:", len(arr))
+}
