d4d0676b9a5655786da55bbf5f41ec158c076852
AnuragRaj11 <Araj59519@gmail.com>
2025-05-23 23:53:14 +0530

    defer_last

---
+++ defer/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+fmt.Println("Hello, World!")
+defer fmt.Println("This is a simple Go program.")
+fmt.Println("defer main function example.")
+}
