90429ad7dc095505db9e454ded4223703fc91036
AnuragRaj11 <Araj59519@gmail.com>
2025-05-25 21:39:18 +0530

    url_shortner

---
+++ Url_Shortner_GoLang/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
+var urlDB = make(map[string]URL)
+
+func generateShortURL(originalURL string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(originalURL))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	return hash[:8]
+}
+
+func createURL(originalURL string) string {
+	shortURL := generateShortURL(originalURL)
+	id := shortURL
+	urlDB[id] = URL{
+		ID:           id,
+		OriginalURL:  originalURL,
+		ShortURL:     shortURL,
+		CreationDate: time.Now(),
+	}
+	return shortURL
+}
+
+func getURL(id string) (URL, error) {
+	url, ok := urlDB[id]
+	if !ok {
+		return URL{}, errors.New("URL not found")
+	}
+	return url, nil
+}
+
+func rootPageURL(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world! Use /shorten to shorten URLs, /redirect/{id} to redirect.")
+}
+
+func shortURLHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		URL string `json:"url"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil || data.URL == "" {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	shortURL := createURL(data.URL)
+	response := struct {
+		ShortURL string `json:"short_url"`
+	}{ShortURL: shortURL}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func redirectURLHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/redirect/"):]
+	url, err := getURL(id)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusNotFound)
+		return
+	}
+	http.Redirect(w, r, url.OriginalURL, http.StatusFound)
+}
+
+func main() {
+	fmt.Println("Starting URL shortener...")
+	OriginalURL := "https://github.com/Prince-1501/"
+    generateShortURL(OriginalURL)
+	http.HandleFunc("/", rootPageURL)
+	http.HandleFunc("/shorten", shortURLHandler)
+	http.HandleFunc("/redirect/", redirectURLHandler)
+
+	fmt.Println("Starting server on port 3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error on starting server:", err)
+	}
+}
